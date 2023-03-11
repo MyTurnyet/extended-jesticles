fc5f91a60e4bfc8bb00623fcb0f1516dd35c7aba
Paige Watson <paige@industriallogic.com>
2023-03-11 10:57:25 -0800

    v1.0.8

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,5 @@
 export * from './expectations';
 export interface JesticleMatchers<R> {
-  //react
   toHaveElementsWithText(...expectedText: string[]): R;
-  //arrays
   toMatchInAnyOrder(expectedArray: any[]): R;
 }
