11c192510ba796247b1d7bdf5ecdf206a58130c3
paige.watson <paige.watson@salesforce.com>
2024-05-24 10:00:57 -0700

    build

--- dist/index.js
+++ dist/index.js
@@ -49,22 +49,17 @@ function toHaveElementsWithText(screen, ...expectedText) {
   return createPassingResult();
 }
 
-// node_modules/@jest/globals/build/index.js
-throw new Error(
-  "Do not import `@jest/globals` outside of the Jest test environment"
-);
-
 // src/expectations/array.expectations.ts
 function toMatchInAnyOrder(actualArray, expectedArray) {
   try {
     expectedArray.forEach((item) => {
-      (void 0)(actualArray).toContainEqual(item);
+      expect(actualArray).toContainEqual(item);
     });
   } catch (e) {
     return createFailingResult("Items in list are not the same");
   }
   try {
-    (void 0)(actualArray.length).toEqual(expectedArray.length);
+    expect(actualArray.length).toEqual(expectedArray.length);
   } catch (e) {
     return createFailingResult("Lengths do not match");
   }
